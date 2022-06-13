bd4376d0882e09beb3529395e20bb5e69cc0287e
KoenigsKind <github@koenigskind.dev>
2022-06-14 01:07:04 +0200

    Make pipes and jump pads layers toggleable

--- js/siu-map.js
+++ js/siu-map.js
@@ -1,23 +1,22 @@
+let mapSize = {width: 8192, height: 8192}
+let pxTrans = {dx: -74515, dy: -85540, m: 21.3678}
+let siuBounds = [
+    [pxTrans.dy, pxTrans.dx],
+    [
+        pxTrans.dy + pxTrans.m * mapSize.height,
+        pxTrans.dx + pxTrans.m * mapSize.width
+    ]
+];
+
 export var SiuMap = L.Map.extend({
+    bounds: siuBounds,
     initialize: function() {
         L.Map.prototype.initialize.call(this, 'map', {
             crs: L.CRS.Simple,
             minZoom: -8
         });
         this.on('contextmenu', function() {/* Do nothing */});
-
-        let mapSize = {width: 8192, height: 8192}
-        let pxTrans = {dx: -74515, dy: -85540, m: 21.3678}
-        let bounds = [
-            [pxTrans.dy, pxTrans.dx],
-            [
-                pxTrans.dy + pxTrans.m * mapSize.height,
-                pxTrans.dx + pxTrans.m * mapSize.width
-            ]
-        ];
-        L.imageOverlay('img/map.jpg', bounds).addTo(this);
-        L.imageOverlay('img/pad_map.png', bounds).addTo(this);
-        L.imageOverlay('img/pipe_map.png', bounds).addTo(this);
-        this.fitBounds(bounds);
+        L.imageOverlay('img/map.jpg', this.bounds).addTo(this);
+        this.fitBounds(this.bounds);
     }
 });
